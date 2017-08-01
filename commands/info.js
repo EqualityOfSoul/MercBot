27a20e225427f7eef39db6fa3aabdf5a706771b8
n0281014 <scott.weaver@libertymutual.com>
2017-08-01 13:00:58 -0400

    Started commands.

--- commands/info.js
+++ commands/info.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 
 module.exports.info = (client, message, args) => {
     message.author.sendEmbed(new Discord.RichEmbed()
-        .setTitle(`I am MercBot`)
+        .setTitle('I am MercBot')
         .setColor(0x44B6EC)
-        .setThumbnail(`https://pbs.twimg.com/profile_images/576946466456928256/C1J_Ki_E_400x400.png`)
+        .setThumbnail('https://pbs.twimg.com/profile_images/576946466456928256/C1J_Ki_E_400x400.png')
         .setTimestamp()
-        .addField(`Information`, `Ryan is worthless.\n`)
+        .addField('Information', 'Ryan is worthless.\n')
     ).catch((err) => {
-        message.reply(`Failed to send Info');
-});
+        message.reply('Failed to send Info');
+    });
 };
