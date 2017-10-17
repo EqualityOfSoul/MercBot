8947763464e06f7169963bf9ac9badb7572fe3dc
Scott Weaver <scott.weaver@libertymutual.com>
2017-10-17 13:22:45 -0400

    Staff

---
+++ commands/staff.js
@@ -0,0 +1,21 @@
+exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
+    message.channel.send(`= STATISTICS =
+• Commander :: Scott
+• Commander :: Akilestar
+• Div Commander :: UNWeaponsInspector
+• Div Commander :: Jimbo
+• Lieutenant   :: SeppukuNinja`)
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "staff",
+    description: "Gives a list of staff members",
+    usage: "staff"
+};
